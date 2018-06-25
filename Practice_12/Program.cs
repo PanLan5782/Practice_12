e6c6fbf5623f2bffbd183e0a74f4246085735215
Никита Лапан <nvlapan@edu.hse.ru>
2018-06-25 13:55:57 +0500

    1.0 Full availability

--- Practice_12/Program.cs
+++ Practice_12/Program.cs
@@ -10,6 +10,153 @@ namespace Practice_12
     {
         static void Main(string[] args)
         {
+            int[] arr = CreateArray();
+            PrintArray(arr);
+            Pyramid_Sort(arr, arr.Length);
+
+
+            PrintArray(arr);
+
+            Console.WriteLine("BucketSort");
+            arr = CreateArray();
+            PrintArray(arr);
+            BucketSort(ref arr);
+            PrintArray(arr);
+            Console.ReadLine();
+        }
+
+        private static void PrintArray(int[] arr)
+        {
+            foreach (int x in arr)
+            {
+                Console.Write(x + " ");
+            }
+            Console.WriteLine();
+        }
+
+        private static int[] CreateArray()
+        {
+            int[] arr = new int[100];
+            //заполняем массив случайными числами
+            Random rd = new Random();
+            for (int i = 0; i < arr.Length; ++i)
+            {
+                arr[i] = rd.Next(1, 101);
+            }
+
+            return arr;
+        }
+
+        static int add2pyramid(int[] arr, int i, int N)
+        {
+            int imax;
+            int buf;
+            if ((2 * i + 2) < N)
+            {
+                if (arr[2 * i + 1] < arr[2 * i + 2]) imax = 2 * i + 2;
+                else imax = 2 * i + 1;
+            }
+            else imax = 2 * i + 1;
+            if (imax >= N) return i;
+            if (arr[i] < arr[imax])
+            {
+                buf = arr[i];
+                arr[i] = arr[imax];
+                arr[imax] = buf;
+                if (imax < N / 2) i = imax;
+            }
+            return i;
+        }
+
+        static void Pyramid_Sort(int[] arr, int len)
+        {
+            //step 1: building the pyramid
+            for (int i = len / 2 - 1; i >= 0; --i)
+            {
+                long prev_i = i;
+                i = add2pyramid(arr, i, len);
+                if (prev_i != i) ++i;
+            }
+
+            //step 2: sorting
+            int buf;
+            for (int k = len - 1; k > 0; --k)
+            {
+                buf = arr[0];
+                arr[0] = arr[k];
+                arr[k] = buf;
+                int i = 0, prev_i = -1;
+                while (i != prev_i)
+                {
+                    prev_i = i;
+                    i = add2pyramid(arr, i, k);
+                }
+            }
+        }
+
+
+        private static void BucketSort(ref int[] items)
+        {
+            // Предварительная проверка элементов исходного массива
+            //
+
+            if (items == null || items.Length < 2)
+                return;
+
+            // Поиск элементов с максимальным и минимальным значениями
+            //
+
+            int maxValue = items[0];
+            int minValue = items[0];
+
+            for (int i = 1; i < items.Length; i++)
+            {
+                if (items[i] > maxValue)
+                    maxValue = items[i];
+
+                if (items[i] < minValue)
+                    minValue = items[i];
+            }
+
+            // Создание временного массива "карманов" в количестве,
+            // достаточном для хранения всех возможных элементов,
+            // чьи значения лежат в диапазоне между minValue и maxValue.
+            // Т.е. для каждого элемента массива выделяется "карман" List<int>.
+            // При заполнении данных "карманов" элементы исходного не отсортированного массива
+            // будут размещаться в порядке возрастания собственных значений "слева направо".
+            //
+
+            List<int>[] bucket = new List<int>[maxValue - minValue + 1];
+
+            for (int i = 0; i < bucket.Length; i++)
+            {
+                bucket[i] = new List<int>();
+            }
+
+            // Занесение значений в пакеты
+            //
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                bucket[items[i] - minValue].Add(items[i]);
+            }
+
+            // Восстановление элементов в исходный массив
+            // из карманов в порядке возрастания значений
+            //
+
+            int position = 0;
+            for (int i = 0; i < bucket.Length; i++)
+            {
+                if (bucket[i].Count > 0)
+                {
+                    for (int j = 0; j < bucket[i].Count; j++)
+                    {
+                        items[position] = bucket[i][j];
+                        position++;
+                    }
+                }
+            }
         }
     }
 }
